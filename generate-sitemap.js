6fb81665b34d1507a5f3b89a699dbbcd2954ed47
brightlane <benitopalmarini@aol.com>
2026-04-16 17:09:36 -0400

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -20,3 +20,22 @@ ${PAGES.map(p => `  <url>
 
 fs.writeFileSync(path.join(ROOT, 'sitemap.xml'), xml, 'utf8');
 console.log(`Wrote sitemap.xml with ${PAGES.length} URLs.`);
+const fs = require('fs');
+const path = require('path');
+
+const ROOT = __dirname;
+const SITE_URL = 'https://brightlane.github.io/booking.com/';
+
+const pages = fs.readdirSync(ROOT)
+  .filter(file => file.endsWith('.html') && file !== '404.html')
+  .map(file => `<url><loc>${SITE_URL}${file}</loc></url>`)
+  .join('\n');
+
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages}
+</urlset>
+`;
+
+fs.writeFileSync(path.join(ROOT, 'sitemap.xml'), xml, 'utf8');
+console.log('sitemap.xml written');
