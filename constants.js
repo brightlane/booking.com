a2f189a8002c1a57b5c686aabb508696f2a66591
brightlane <benitopalmarini@aol.com>
2026-04-16 13:07:49 -0400

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,16 @@
+// constants.js
+module.exports = {
+  BOOKING_ROOT: "https://www.booking.com",
+  AID: "8132800",
+
+  bookingAffiliateUrls: [
+    "https://www.booking.com/index.html?aid=8132800",
+    "https://www.booking.com/apartments/index.html?aid=8132800",
+    "https://www.booking.com/resorts/index.html?aid=8132800",
+    "https://www.booking.com/villas/index.html?aid=8132800",
+    "https://www.booking.com/bed-and-breakfast/index.html?aid=8132800",
+    "https://www.booking.com/guest-house/index.html?aid=8132800",
+  ],
+
+  skyscannerAffiliateUrl: "https://convert.ctypy.com/aff_c?offer_id=29465&aff_id=21885",
+};
